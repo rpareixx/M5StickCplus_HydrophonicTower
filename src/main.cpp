0854ac3502bf479ec71e8fc935b983482088ab62
Robert Parei <robert.parei@parei.eu>
2023-10-07 21:43:38 +0200

    OTA integration

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,33 @@
 // WiFi reconnect configurieren
 
 #include <Arduino.h>
+#include <GitHubFsOTA.h>
+#include <GitHubOTA.h>
 #include <M5StickCPlus.h>
 #include <WiFi.h>
 #include <secrets.h>
+
+#include "LittleFS.h"
+
+// This string should correspond to github tag used for Releasing (via. Github
+// Actions)
+#define VERSION "0.0.1"
+
+#define RELEASE_URL                                                       \
+    "https://github.com/rpareixx/M5StickCplus_HydrophonicTower/releases/" \
+    "latest"
+
+#define DELAY_MS 1000
 
 char ssid[] = SECRET_CASTEL_SSID;
 char password[] = SECRET_CASTEL_PASS;
 
 #define CONNECTION_TIMEOUT 10
+
+#define LED_BUILTIN 10
+
+GitHubOTA OsOta(VERSION, RELEASE_URL, "firmware.bin", true);
+GitHubFsOTA FsOta(VERSION, RELEASE_URL, "filesystem.bin", true);
 
 IPAddress local_IP(192, 168, 1, 48);
 IPAddress primaryDNS(8, 8, 8, 8);    // optional
@@ -18,26 +37,55 @@ IPAddress secondaryDNS(8, 8, 4, 4);  // optional
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 254, 1);
 
-void get_network_info() {
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("[*] Network information for ");
-        Serial.println(ssid);
-
-        Serial.println("[+] BSSID : " + WiFi.BSSIDstr());
-        Serial.print("[+] Gateway IP : ");
-        Serial.println(WiFi.gatewayIP());
-        Serial.print("[+] Subnet Mask : ");
-        Serial.println(WiFi.subnetMask());
-        Serial.println((String) "[+] RSSI : " + WiFi.RSSI() + " dB");
-        Serial.print("[+] ESP32 IP : ");
-        Serial.println(WiFi.localIP());
-    }
-}
+void setup_wifi();
+void listRoot();
+void get_network_info();
 
 void setup() {
     M5.begin();
     M5.Lcd.setRotation(3);
 
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    Serial.printf("Current firmware version: %s\n", VERSION);
+    Serial.printf("Current filesystem version: %s\n", VERSION);
+
+    LittleFS.begin();
+    listRoot();
+
+    setup_wifi();
+
+    // Chech for updates
+    FsOta.handle();
+    OsOta.handle();
+}
+
+void loop() {
+    // Wenn Sie die Button-Klasse verwenden, rufen Sie immer Update auf, um den
+    // Status zu aktualisieren M5.update();
+
+    // Your code goes here
+    digitalWrite(LED_BUILTIN,
+                 HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(DELAY_MS);     // wait for a second
+    digitalWrite(LED_BUILTIN,
+                 LOW);  // turn the LED off by making the voltage LOW
+    delay(DELAY_MS);    // wait for a second
+}
+
+void listRoot() {
+    Serial.printf("Listing root directory\r\n");
+
+    File root = LittleFS.open("/", "r");
+    File file = root.openNextFile();
+
+    while (file) {
+        Serial.printf("  FILE: %s\r\n", file.name());
+        file = root.openNextFile();
+    }
+}
+
+void setup_wifi() {
     // Configures static IP address
     if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
         Serial.println("STA Failed to configure");
@@ -71,7 +119,18 @@ void setup() {
     M5.Lcd.println(WiFi.localIP());
 }
 
-void loop() {
-    // Wenn Sie die Button-Klasse verwenden, rufen Sie immer Update auf, um den
-    // Status zu aktualisieren M5.update();
+void get_network_info() {
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.println("[*] Network information for ");
+        Serial.println(ssid);
+
+        Serial.println("[+] BSSID : " + WiFi.BSSIDstr());
+        Serial.print("[+] Gateway IP : ");
+        Serial.println(WiFi.gatewayIP());
+        Serial.print("[+] Subnet Mask : ");
+        Serial.println(WiFi.subnetMask());
+        Serial.println((String) "[+] RSSI : " + WiFi.RSSI() + " dB");
+        Serial.print("[+] ESP32 IP : ");
+        Serial.println(WiFi.localIP());
+    }
 }
